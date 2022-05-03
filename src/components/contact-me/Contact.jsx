2b1c720e62c8218404a4dc1ae4ebf66be4dd3cc7
Denis Arce <dnsarce21@gmail.com>
2022-05-03 15:48:45 -0500

    updating contact section and fixing errors

--- src/components/contact-me/Contact.jsx
+++ src/components/contact-me/Contact.jsx
@@ -1,6 +1,13 @@
 import "./contact.scss";
 import contact from "../img/contact.png";
+import { useState } from "react";
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+
   return (
     <div className="contact">
       <div className="left">
@@ -8,10 +15,11 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact Me!</h2>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="email" />
           <textarea placeholder="message"></textarea>
           <button type="submit">Send Message</button>
+          {message && <span>Thank you, will get back to you soon!</span>}
         </form>
       </div>
     </div>
